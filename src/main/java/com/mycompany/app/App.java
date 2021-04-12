6e136f07ce272e93e8686b9f8190e79a15457f46
Nhan Phan <phannhan.dn@gmail.com>
2021-04-12 22:49:03 +0700

    Update

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -2,12 +2,14 @@ package com.mycompany.app;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = x++;
+        System.out.println("X = "+ x);
+        System.out.println("Y = "+ y);
+        System.out.println("Hello World!");
+
     }
 }
